e82dcfb04ace161600d70b3675452e69059b3396
geciu <getekf@gmail.com>
2019-04-04 12:38:26 +0200

    update

--- src/zajecia1/Main.java
+++ src/zajecia1/Main.java
@@ -12,17 +12,19 @@ public class Main {
         System.out.println(" ");
 
         //Zadanie 2
-     //   System.out.println("Zadanie 2");
-    //    Liczby liczby = new Liczby();
+        System.out.println("Zadanie 2");
+        Liczby liczby = new Liczby();
 
         //Zadanie 3
-    //    System.out.println("Zadanie 3");
-   //     BMI bmi = new BMI();
+        System.out.println("Zadanie 3");
+        BMI bmi = new BMI();
 
         //Zadanie 4
-  //      System.out.println("Zadanie 4");
-      //  Podatek podatek = new Podatek();
+        System.out.println("Zadanie 4");
+        Podatek podatek = new Podatek();
 
+        //Zadanie 5
+        System.out.println("Zadanie 5");
         AGD agd = new AGD();
     }
 }
